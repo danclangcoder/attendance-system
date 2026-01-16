e9ffe3e03f22a96afef8e3df537dc1cbcd772f17
danclangcoder <nielbandahala@gmail.com>
2026-01-16 21:55:32 +0800

    Registration process logic

--- tests/sha_256.py
+++ tests/sha_256.py
@@ -5,28 +5,30 @@ def hash_key(data: str):
         print('Does not contain a valid string')
     
     else:
-        new_hash = hashlib.new('sha')
+        new_hash = hashlib.new('sha256')
         new_hash.update(data.encode())
         new_key = new_hash.hexdigest()
         
         return new_key
 
 if __name__ == '__main__':
-    # Example: qwerty123 -> daaad6e5604e8e17bd9f108d91e26afe6281dac8fda0091040a7a6d7bd9b43b5
-    hashes = [
-        'daaad6e5604e8e17bd9f108d91e26afe6281dac8fda0091040a7a6d7bd9b43b5', 
-        '2fadff0c979214ed48713033fc9d83bbaa0fb7091a665d281f61e07148d97f9b'
-    ]
-    student_id = ('qwerty123')
-    my_key = hash_key(student_id)
-
-    if my_key not in hashes:
-        print(f'NEW {my_key}')
-
-        '''
-        - Append keys locally or to Sheets
-        - Best practice: keep keys in a JSON file
-        '''
+    students = {
+        "A123F0024": "bc0cd7c2cebfdde4147fe5c3a517a3c46fdcdd239f2436927f290d317a450d10"
+    }
     
-    else:
-        print(f'FOUND {my_key}')
+    in_menu = True
+
+    while in_menu:
+        char = input('Enter to REGISTER and q or Q to QUIT: ')
+        if char == 'q' or 'Q':
+            break
+
+        student_number = input('Enter Student No: ')
+        new_hash = hash_key(student_number)
+
+        if new_hash not in students.values():
+            print(f'NEW {new_hash}')
+            students[student_number] = new_hash
+        
+        else:
+            print(f'FOUND {new_hash}')
