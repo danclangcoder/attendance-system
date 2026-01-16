ed2f0a08f2f245a55b82a71f3df4e1339c1aac05
danclangcoder <nielbandahala@gmail.com>
2026-01-16 18:00:40 +0800

    Code cleanup and refactor

--- tests/main.py
+++ tests/main.py
@@ -4,9 +4,5 @@
     Put test code here, not in src.
 '''
 
-import cv2, time
-from pyzbar.pyzbar import decode
-
-
 if __name__ == '__main__':
     print('Running app... Please wait.')
--- tests/sha_256.py
+++ tests/sha_256.py
@@ -5,17 +5,28 @@ def hash_key(data: str):
         print('Does not contain a valid string')
     
     else:
-        new_hash = hashlib.new('sha256')
+        new_hash = hashlib.new('sha')
         new_hash.update(data.encode())
         new_key = new_hash.hexdigest()
         
         return new_key
 
 if __name__ == '__main__':
-    # student id = qwerty123
-    hashes = ['daaad6e5604e8e17bd9f108d91e26afe6281dac8fda0091040a7a6d7bd9b43b5']
+    # Example: qwerty123 -> daaad6e5604e8e17bd9f108d91e26afe6281dac8fda0091040a7a6d7bd9b43b5
+    hashes = [
+        'daaad6e5604e8e17bd9f108d91e26afe6281dac8fda0091040a7a6d7bd9b43b5', 
+        '2fadff0c979214ed48713033fc9d83bbaa0fb7091a665d281f61e07148d97f9b'
+    ]
     student_id = ('qwerty123')
     my_key = hash_key(student_id)
-    print(f'SHA256 -> {my_key}')
-    for hash in hashes:
-        print(hash == my_key)
+
+    if my_key not in hashes:
+        print(f'NEW {my_key}')
+
+        '''
+        - Append keys locally or to Sheets
+        - Best practice: keep keys in a JSON file
+        '''
+    
+    else:
+        print(f'FOUND {my_key}')
